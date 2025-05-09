a3b71a19178196da6748869263c0923401bc4b49
aantnn <anton@valishin.ru>
2025-05-09 03:20:01 +0300

    initial: make build in windows and linux

--- nativevpn/src/main/cpp/cmake/modules/CommonAndroidSetup.cmake
+++ nativevpn/src/main/cpp/cmake/modules/CommonAndroidSetup.cmake
@@ -14,15 +14,26 @@ endfunction()
 
 get_autoconf_target(AUTOCONF_TARGET)
 
-set(android_env "ANDROID_NDK_ROOT=${ANDROID_NDK}"
-    "CC=${ANDROID_TOOLCHAIN_ROOT}/bin/${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang"
-    "AR=${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-ar"
-    "AS=${ANDROID_TOOLCHAIN_ROOT}/bin/${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang"
-    "CXX=${ANDROID_TOOLCHAIN_ROOT}/bin/${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang++"
-    "LD=${ANDROID_TOOLCHAIN_ROOT}/bin/ld"
-    "RANLIB=${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-ranlib"
-    "STRIP=${ANDROID_TOOLCHAIN_ROOT}/bin/llvm-strip"
-    "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
+if(CMAKE_HOST_WIN32)
+    set(ENV_SETTER "set")
+else ()
+    set(ENV_SETTER "export")
+endif()
+
+set(android_env "${ENV_SETTER} ANDROID_NDK_ROOT=${ANDROID_NDK} \n")
+set(android_env "${android_env} ${ENV_SETTER} CC=${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang \n")
+set(android_env "${android_env} ${ENV_SETTER} AR=llvm-ar \n")
+set(android_env "${android_env} ${ENV_SETTER} AS=${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang \n")
+set(android_env "${android_env} ${ENV_SETTER} CXX=${AUTOCONF_TARGET}${ANDROID_NATIVE_API_LEVEL}-clang++ \n")
+set(android_env "${android_env} ${ENV_SETTER} LD=ld \n")
+set(android_env "${android_env} ${ENV_SETTER} RANLIB=llvm-ranlib \n")
+set(android_env "${android_env} ${ENV_SETTER} STRIP=llvm-strip \n")
+if(CMAKE_HOST_WIN32)
+    set(android_env "${android_env} ${ENV_SETTER} PATH=${ANDROID_TOOLCHAIN_ROOT}/bin;$ENV{PATH} \n")
+else ()
+    set(android_env "${android_env} ${ENV_SETTER} PATH=\"${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}\" \n")
+endif()
+
 
 function(log_error error command args dir )
     string(REPLACE ";" "\ " command_print "${command}")
@@ -37,7 +48,8 @@ endfunction()
 function(build_external target src_dir)
     message(STATUS "Building external project: ${target} in: ${src_dir} ")
     set(trigger_build_dir "${src_dir}")
-
+    # Generate a batch file to handle the PATH properly
+    #"${CMAKE_BINARY_DIR}/configure_env.bat"
     set(CMAKE_LIST_CONTENT "
         cmake_minimum_required(VERSION 3.22.1)
         project(${target})
@@ -91,16 +103,38 @@ function(build_autoconf_external_project project source_dir env configure_cmd bu
             #-DCMAKE_C_FLAGS="${CMAKE_C_FLAGS}"
             -G${CMAKE_GENERATOR}
     )
+    string(REPLACE "\\" "/" CMAKE_ARGS "${CMAKE_ARGS}")
     message(STATUS "NECESSARY Copy reason: BUILD_IN_SOURCE 1. From ${project} sources to make by Superbuild ExternalProject to ${AUTOCONF_CURRENT_BUILD_DIR}")
     file(COPY "${source_dir}" DESTINATION "${AUTOCONF_CURRENT_BUILD_DIR}/..")
+    # The %PATH% semicolon issue in ExternalProject_Add is particularly stubborn on Windows so
+    # Determine host platform and create appropriate environment script
+    if(CMAKE_HOST_WIN32)
+        # Windows host - create batch script
+        file(WRITE ${CMAKE_BINARY_DIR}/configure_env.bat
+                "@echo off
+                setlocal enabledelayedexpansion
+                ${android_env}
+                ${CMAKE_COMMAND} -E env %*" )
+        set(ENV_SCRIPT_CMD "${CMAKE_BINARY_DIR}/configure_env.bat")
+    else()
+        # Linux/Mac host - create bash script
+        file(WRITE ${CMAKE_BINARY_DIR}/configure_env.sh
+                "#!/bin/bash
+                ${android_env}
+                ${CMAKE_COMMAND} -E env $@"
+exit $?")
+        execute_process(COMMAND chmod +x ${CMAKE_BINARY_DIR}/configure_env.sh)
+        set(ENV_SCRIPT_CMD "${CMAKE_BINARY_DIR}/configure_env.sh")
+    endif()
+
     build_external(
             "${project}_${ANDROID_ABI}"
             ${AUTOCONF_CURRENT_BUILD_DIR}
-            " SOURCE_DIR ${AUTOCONF_CURRENT_BUILD_DIR} "
+            " SOURCE_DIR        ${AUTOCONF_CURRENT_BUILD_DIR} "
             " BUILD_IN_SOURCE 1 "
-            " CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env ${android_env} ${configure_cmd}"
-            " BUILD_COMMAND ${CMAKE_COMMAND} -E env ${android_env} make -j${NPROC} ${build_args} "
-            " INSTALL_COMMAND ${CMAKE_COMMAND} -E env ${android_env} make -j${NPROC} ${install_args} "
-            " CMAKE_ARGS ${CMAKE_ARGS} "
+            " CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env ${ENV_SCRIPT_CMD} ${configure_cmd}"
+            " BUILD_COMMAND     ${CMAKE_COMMAND} -E env ${ENV_SCRIPT_CMD} make -j${NPROC} ${build_args} "
+            " INSTALL_COMMAND   ${CMAKE_COMMAND} -E env ${ENV_SCRIPT_CMD} make -j${NPROC} ${install_args} "
+            " CMAKE_ARGS        ${CMAKE_ARGS} "
     )
 endfunction()
